e37cae4c2a7ab7b03ac98b5c41356d05e334e671
cya <cya990524@gmail.com>
2020-06-29 20:28:50 +0800

    add frontend dependence echart and v-charts

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,12 +4,14 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import axios from 'axios'
+import VCharts from 'v-charts'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
 Vue.prototype.$axios = axios
 Vue.use(ElementUI)
+Vue.use(VCharts)
 
 /* eslint-disable no-new */
 new Vue({
